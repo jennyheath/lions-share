a845a3be3e48755387d93adab05c42aabbe05194
Sofie Garden <sofie.garden@gmail.com>
2015-07-16 19:50:43 -0700

    Complete schema

--- lions_share/db/migrate/20150717021408_create_listings.rb
+++ lions_share/db/migrate/20150717021408_create_listings.rb
@@ -1,7 +1,7 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-      t.string :type
+      t.string :type_of
       t.decimal :price
       t.decimal :bed_count
       t.decimal :bath_count
--- lions_share/db/migrate/20150717022814_create_photos.rb
+++ lions_share/db/migrate/20150717022814_create_photos.rb
@@ -2,6 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :url
+      t.belongs_to :listing
 
       t.timestamps null: false
     end
