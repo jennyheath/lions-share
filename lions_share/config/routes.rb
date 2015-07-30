70bfbf33408d3eb9da07c7379afe868128d3b870
Sofie Garden <sofie.garden@gmail.com>
2015-07-30 10:38:24 -0700

    Add welcome controller

--- lions_share/config/routes.rb
+++ lions_share/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :listings
+  resources :listings, only: [:index, :show]
 
   # Example resource route with options:
   #   resources :products do
@@ -48,9 +48,9 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :admin do
+    # Directs /admin/listings/* to Admin::ListingsController
+    # (app/controllers/admin/listings_controller.rb)
+    resources :listings
+  end
 end
