7d6c75c0174ed4d12fb3530bb617dd59a0b09ff3
jennyheath <jennifer.w.heath@gmail.com>
2015-09-09 23:52:58 -0700

    create admin namespace and controller

---
+++ app/controllers/admin/listings_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ListingsController < ApplicationController
+  def new
+    @listing = Listing.new
+    @brokers = Broker.all
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @brokers = Broker.all
+
+    if @listing.save
+      render :saved
+    else
+      flash.now[:errors] = @listing.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @listings = Listing.all
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    if @listing.update_attributes(listing_params)
+      render :saved
+    else
+      flash[:errors] = @listing.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    flash[:notice] = ["Listing deleted"]
+    @listing = Listing.new
+    render :new
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:type_of, :price, :bed_count, :bath_count, :neighborhood, :featured, :address, :description, :exclusive, :no_fee)
+  end
+end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -67,63 +67,15 @@ class ListingsController < ApplicationController
     end
   end
 
-  def new
-  end
-
   def show
     @listing = Listing.find(params[:id])
   end
 
-  def create
-    @listing = Listing.new(listing_params)
-
-    if listing.save
-      redirect_to @listing
-    else
-      # error handling
-    end
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def delete
-
-  end
-
   def rentals
     render 'rentals'
-
-    # This code was grabbed from the article I sent as well -- not fully functional yet
-    @filterrific = Filterrific.new(Listing, params[:filterrific])
-    @filterrific.select_options = {
-      sorted_by: Listing.options_for_sorted_by,
-      with_neighborhood: Listing.options_for_select,
-      with_bed: Listing.options_for_select,
-      with_price: Listing.options_for_select,
-      # This will likely have to change, again because amenities is going to be more complicated -- it is a separate model entirely
-      with_amenities: Listing.options_for_select
-    }
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   def sales
     render 'sales'
   end
-
-  private
-
-  def listing_params
-    params.require(:listing).permit(:type_of, :price, :bed_count, :bath_count, :neighborhood, :featured, :address, :description, :exclusive, :no_fee)
-  end
-
 end
